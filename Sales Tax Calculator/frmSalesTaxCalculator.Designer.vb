197479e8ca58f23af6cc00583d6f8c3c996f44f8
Abbie <Abbie@LAPTOP-P0CANOAH>
2020-02-07 12:45:27 -0500

    Add project files.

---
+++ Sales Tax Calculator/frmSalesTaxCalculator.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSalesTaxCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.lblItemName = New System.Windows.Forms.Label()
+        Me.lblItemCost = New System.Windows.Forms.Label()
+        Me.btnDisplayCost = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtItemName = New System.Windows.Forms.TextBox()
+        Me.txtItemCost = New System.Windows.Forms.TextBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblSalesTax = New System.Windows.Forms.Label()
+        Me.lblTotalCost = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Century Gothic", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.Location = New System.Drawing.Point(98, 20)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(217, 25)
+        Me.lblHeading.TabIndex = 0
+        Me.lblHeading.Text = "Sales Tax Calculator"
+        '
+        'lblItemName
+        '
+        Me.lblItemName.AutoSize = True
+        Me.lblItemName.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblItemName.Location = New System.Drawing.Point(54, 128)
+        Me.lblItemName.Name = "lblItemName"
+        Me.lblItemName.Size = New System.Drawing.Size(120, 21)
+        Me.lblItemName.TabIndex = 2
+        Me.lblItemName.Text = "Name of item:"
+        '
+        'lblItemCost
+        '
+        Me.lblItemCost.AutoSize = True
+        Me.lblItemCost.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblItemCost.Location = New System.Drawing.Point(54, 192)
+        Me.lblItemCost.Name = "lblItemCost"
+        Me.lblItemCost.Size = New System.Drawing.Size(108, 21)
+        Me.lblItemCost.TabIndex = 3
+        Me.lblItemCost.Text = "Cost of item:"
+        '
+        'btnDisplayCost
+        '
+        Me.btnDisplayCost.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDisplayCost.Location = New System.Drawing.Point(43, 365)
+        Me.btnDisplayCost.Name = "btnDisplayCost"
+        Me.btnDisplayCost.Size = New System.Drawing.Size(115, 23)
+        Me.btnDisplayCost.TabIndex = 4
+        Me.btnDisplayCost.Text = "Display Cost"
+        Me.btnDisplayCost.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnClear.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(179, 365)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(94, 23)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(303, 365)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(100, 23)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtItemName
+        '
+        Me.txtItemName.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtItemName.Location = New System.Drawing.Point(179, 125)
+        Me.txtItemName.Multiline = True
+        Me.txtItemName.Name = "txtItemName"
+        Me.txtItemName.Size = New System.Drawing.Size(100, 24)
+        Me.txtItemName.TabIndex = 8
+        '
+        'txtItemCost
+        '
+        Me.txtItemCost.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtItemCost.Location = New System.Drawing.Point(179, 192)
+        Me.txtItemCost.Name = "txtItemCost"
+        Me.txtItemCost.Size = New System.Drawing.Size(68, 27)
+        Me.txtItemCost.TabIndex = 9
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(97, 60)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(218, 16)
+        Me.lblTitle.TabIndex = 10
+        Me.lblTitle.Text = "The tax rate for all items is 8.75%"
+        '
+        'lblSalesTax
+        '
+        Me.lblSalesTax.AutoSize = True
+        Me.lblSalesTax.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSalesTax.Location = New System.Drawing.Point(54, 248)
+        Me.lblSalesTax.Name = "lblSalesTax"
+        Me.lblSalesTax.Size = New System.Drawing.Size(83, 21)
+        Me.lblSalesTax.TabIndex = 11
+        Me.lblSalesTax.Text = "Sales Tax:"
+        '
+        'lblTotalCost
+        '
+        Me.lblTotalCost.AutoSize = True
+        Me.lblTotalCost.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalCost.Location = New System.Drawing.Point(54, 302)
+        Me.lblTotalCost.Name = "lblTotalCost"
+        Me.lblTotalCost.Size = New System.Drawing.Size(93, 21)
+        Me.lblTotalCost.TabIndex = 12
+        Me.lblTotalCost.Text = "Total Cost:"
+        '
+        'frmSalesTaxCalculator
+        '
+        Me.AcceptButton = Me.btnDisplayCost
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnClear
+        Me.ClientSize = New System.Drawing.Size(443, 419)
+        Me.Controls.Add(Me.lblTotalCost)
+        Me.Controls.Add(Me.lblSalesTax)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.txtItemCost)
+        Me.Controls.Add(Me.txtItemName)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDisplayCost)
+        Me.Controls.Add(Me.lblItemCost)
+        Me.Controls.Add(Me.lblItemName)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Name = "frmSalesTaxCalculator"
+        Me.Text = "Sales Tax Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents lblItemName As Label
+    Friend WithEvents lblItemCost As Label
+    Friend WithEvents btnDisplayCost As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtItemName As TextBox
+    Friend WithEvents txtItemCost As TextBox
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblSalesTax As Label
+    Friend WithEvents lblTotalCost As Label
+End Class
---
+++ Sales Tax Calculator/frmSalesTaxCalculator.vb
@@ -0,0 +1,51 @@
+﻿' Program:  City Bike Rental
+' Author:   Corinne Hoisington
+' Date:     April 14, 2017
+' Purpose:  This application calculates and displays 
+'           the total cost of renting multiple bikes for a 24-hour period.
+
+Option Strict On
+
+Public Class frmSalesTaxCalculator
+
+    Const _cdecTax As Decimal = 0.0875D
+
+    Private Sub btnDisplayCost_Click(sender As Object, e As EventArgs) Handles btnDisplayCost.Click
+        Dim ItemCost As Decimal
+        Dim SalesTax As Decimal
+        Dim TotalCost As Decimal
+
+        ' Input
+        Decimal.TryParse(txtItemCost.Text, ItemCost)
+
+        ' Processing
+        SalesTax = CDec(ItemCost * 0.0875)
+        TotalCost = SalesTax + ItemCost
+
+        ' Output
+        lblSalesTax.Text = SalesTax.ToString("C")
+        lblTotalCost.Text = TotalCost.ToString("C")
+    End Sub
+
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear values
+        txtItemName.Clear()
+        txtItemCost.Clear()
+        lblSalesTax.Text = ""
+        lblTotalCost.Text = ""
+        txtItemName.Focus()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+
+    Private Sub frmSalesTaxCalculator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub lblCost_Click(sender As Object, e As EventArgs) Handles lblItemCost.Click
+
+    End Sub
+End Class
